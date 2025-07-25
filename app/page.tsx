c5e9f1f62a6b218bd1248b0a126a19d5ba4a81a4
sevelater <selmeczi.vilmos.vazul@gmail.com>
2025-07-25 20:40:41 +0200

    maybe

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,15 @@
 import React from 'react';
 import Tabs from "./components/tabs/page";
 
-export const Page = () => {
+export default function Page() {
   return (
     <div className="bg-[var(--first)] h-screen overflow-hidden">
       <div className="w-full">
         {/* <img src="/jenoi_kep.jpg" alt=""></img> */}
       </div>
       <main className="w-full m-10">
-        <Tabs/>
+        <Tabs />
       </main>
-
     </div>
-  )
+  );
 }
-
-
-export default Page;
